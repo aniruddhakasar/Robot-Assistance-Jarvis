a9f945b97f928f898bee1a8cd917dedb6d5832fa
aniruddhakasar <aniruddhakasar2001@gmail.com>
2023-11-23 11:33:23 +0530

    first

---
+++ Code/AI Jarvis Using Python Tut/Jarvis.py
@@ -0,0 +1,45 @@
+from Brain.AIBrain import ReplyBrain
+from Brain.Qna import QuestionsAnswer
+from Body.Listen import MicExecution
+print(">> Starting The Jarvis : Wait For Some Seconds.")
+from Body.Speak import Speak
+from Features.Clap import Tester
+print(">> Starting The Jarvis : Wait For Few Seconds More")
+
+def MainExecution():
+
+    Speak("Hello Sir")
+    Speak("I'm Jarvis, I'm Ready To Assist You Sir.")
+
+    while True:
+
+        Data = MicExecution()
+        Data = str(Data)
+
+        if len(Data)<3:
+            pass
+
+        elif "turn on the tv" in Data:# Specific COmmand
+            Speak("Ok..Turning On The Android TV")
+
+        elif "what is" in Data or "where is" in Data or "question" in Data or "answer" in Data:
+            Reply = QuestionsAnswer(Data)
+
+        else:
+            Reply = ReplyBrain(Data)
+            Speak(Reply)
+
+def ClapDetect():
+
+    query = Tester()
+    if "True-Mic" in query:
+        print("")
+        print(">> Clap Detected!! >>")
+        print("")
+        MainExecution()
+    else:
+        pass
+
+ClapDetect()
+
+# 10% 
